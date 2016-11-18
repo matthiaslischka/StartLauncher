082de23109f7bafb7832ad09f3d626b14915c2ac
Matthias Lischka <mli@techtalk.at>
2016-11-18 18:22:49 +0100

    Added simple FileLogger

--- StartLauncher.App/App.xaml.cs
+++ StartLauncher.App/App.xaml.cs
@@ -9,5 +9,13 @@ namespace StartLauncher.App
             base.OnStartup(e);
             CommandsPopulator.GetInstance().EnsureCommands();
         }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred.\nPlease report this issue to https://github.com/matthiaslischka/StartLauncher\nThx for contributing.\n\nFind the logfile in the application folder.\n\n:)", "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            FileLogger.GetInstance().Logger(e.Exception);
+            e.Handled = true;
+            Current.Shutdown();
+        }
     }
 }
---
+++ StartLauncher.App/FileLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace StartLauncher.App
+{
+    public class FileLogger
+    {
+        private static FileLogger _fileLogger;
+
+        private FileLogger()
+        {
+        }
+
+        public static FileLogger GetInstance()
+        {
+            return _fileLogger ?? (_fileLogger = new FileLogger());
+        }
+
+        public void Logger(Exception exception)
+        {
+            var file = new StreamWriter("errorlog.txt", true);
+            file.WriteLine($"[{DateTime.Now}]");
+            file.WriteLine(exception);
+
+            file.Close();
+        }
+    }
+}
