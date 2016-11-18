15fc1a11d3d02189cf0b22071f965d98d9dc3ff5
Matthias Lischka <mli@techtalk.at>
2016-11-18 23:19:08 +0100

    User specific start menu and directory cleanup

--- StartLauncher.App/CommandsPopulator.cs
+++ StartLauncher.App/CommandsPopulator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using IWshRuntimeLibrary;
 
 namespace StartLauncher.App
@@ -13,17 +14,32 @@ namespace StartLauncher.App
 
         public static CommandsPopulator Current { get; } = new CommandsPopulator();
 
+        private static DirectoryInfo CommandsDirectory { get; } = new DirectoryInfo("Commands");
+
+        private static DirectoryInfo AppStartMenuDirectory
+        {
+            get
+            {
+                var commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+                return new DirectoryInfo(Path.Combine(commonStartMenuPath, "Programs", "Start Launcher"));
+            }
+        }
+
         public void EnsureCommands()
         {
             DataAccessor.Current.ReloadCommands();
+
+            ClearFolder(CommandsDirectory);
+            ClearFolder(AppStartMenuDirectory);
+
             foreach (var command in DataAccessor.Current.Commands)
             {
-                var commandFileInfo = command.GetCommandFileInfo();
+                var commandFileInfo = GetCommandFileInfo(command);
 
                 if (commandFileInfo.Exists)
                     commandFileInfo.Delete();
 
-                Directory.CreateDirectory(commandFileInfo.DirectoryName);
+                Directory.CreateDirectory(CommandsDirectory.FullName);
 
                 var streamWriter = new StreamWriter(commandFileInfo.FullName);
                 streamWriter.WriteLine("echo off");
@@ -34,24 +50,46 @@ namespace StartLauncher.App
             }
         }
 
+
+        public static FileInfo GetCommandFileInfo(CommandDto command)
+        {
+            var commandFileName = command.Name + ".bat";
+            var commandRelativePath = Path.Combine(CommandsDirectory.FullName, commandFileName);
+            return new FileInfo(commandRelativePath);
+        }
+
+        private static void ClearFolder(DirectoryInfo directory)
+        {
+            if (!directory.Exists)
+                return;
+
+            foreach (var file in directory.GetFiles())
+                file.Delete();
+
+            foreach (var dir in directory.GetDirectories())
+                dir.Delete(true);
+        }
+
         private static void AddShortcutForCommand(CommandDto command)
         {
-            var commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
-            var appStartMenuPath = Path.Combine(commonStartMenuPath, "Programs", "Start Launcher");
+            if (!AppStartMenuDirectory.Exists)
+            {
+                var securityRules = new DirectorySecurity();
+                securityRules.AddAccessRule(new FileSystemAccessRule(Environment.UserName, FileSystemRights.FullControl,
+                    AccessControlType.Allow));
+                AppStartMenuDirectory.Create(securityRules);
+            }
 
-            if (!Directory.Exists(appStartMenuPath))
-                Directory.CreateDirectory(appStartMenuPath);
-
-            var shortcutLocation = Path.Combine(appStartMenuPath, command.Name + ".lnk");
+            var shortcutFileInfo = new FileInfo(Path.Combine(AppStartMenuDirectory.FullName, command.Name + ".lnk"));
 
             var shell = new WshShell();
             try
             {
-                var shortcut = shell.CreateShortcut(shortcutLocation);
+                var shortcut = shell.CreateShortcut(shortcutFileInfo.FullName);
                 try
                 {
                     shortcut.Description = command.Description;
-                    shortcut.TargetPath = command.GetCommandFileInfo().FullName;
+                    shortcut.TargetPath = GetCommandFileInfo(command).FullName;
                     shortcut.Save();
                 }
                 finally
