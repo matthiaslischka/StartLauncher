a17b1a604deafaa26aad7102e08549c294b86ea9
Matthias Lischka <mli@techtalk.at>
2016-11-25 17:49:28 +0100

    User Configuration init

---
+++ StartLauncher.App/Properties/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace StartLauncher.App.Properties
+{
+    internal sealed partial class Settings
+    {
+        public Settings()
+        {
+            if (string.IsNullOrEmpty(commandsJsonFilePath))
+                commandsJsonFilePath = Path.Combine(Application.UserAppDataPath, "commands.json");
+        }
+    }
+}
