88668c66042b35dca9b89b3121a7771abec59d30
Matthias Lischka <mli@techtalk.at>
2016-12-01 23:41:53 +0100

    Testproject and simple TestIconResolver Issue #1 Tests

---
+++ StartLauncher.Tests/IconResolverTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StartLauncher.App.Core;
+using StartLauncher.App.DataAccess;
+
+namespace StartLauncher.Tests
+{
+    [TestClass]
+    public class IconResolverTests
+    {
+        [TestMethod]
+        public void TryResolveIconUrl_WithInvalidCharacter_DoesNotThrow()
+        {
+            var commandDtoWithInvalidCharacterCommand = new CommandDto {Command = "\""};
+            Action tryResolveIconUrl = () => { IconResolver.TryResolveIconUrl(commandDtoWithInvalidCharacterCommand); };
+            tryResolveIconUrl.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void TryResolveIconUrl_WithInvalidCharacter_ReturnsNone()
+        {
+            var commandDtoWithInvalidCharacterCommand = new CommandDto {Command = "\""};
+            var tryResolveIconUrlReslt = IconResolver.TryResolveIconUrl(commandDtoWithInvalidCharacterCommand);
+            tryResolveIconUrlReslt.IsNone.Should().BeTrue();
+        }
+    }
+}
