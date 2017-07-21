f3022bacb2d9b9ff58dbb2fd0bfbe34ea5c3cfd7
Matthias Lischka <matthias.lischka@gmx.at>
2017-07-21 21:42:04 +0200

    settings view to mvvm

---
+++ StartLauncher.App/ViewModels/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace StartLauncher.App.ViewModels
+{
+  public class DelegateCommand<T> : ICommand
+  {
+    private readonly Action<T> _action;
+
+    public DelegateCommand(Action<T> action)
+    {
+      _action = action;
+    }
+
+    public void Execute(object parameter)
+    {
+      _action((T)parameter);
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      return true;
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+      add { }
+      remove { }
+    }
+  }
+}
---
+++ StartLauncher.App/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using StartLauncher.App.DataAccess;
+
+namespace StartLauncher.App.ViewModels
+{
+  public class SettingsViewModel : INotifyPropertyChanged
+  {
+    private readonly ICommandsDataAccessor _commandsDataAccessor;
+
+    private string _commandsJsonFilePath;
+
+    public SettingsViewModel(ICommandsDataAccessor commandsDataAccessor)
+    {
+      _commandsDataAccessor = commandsDataAccessor;
+      _commandsJsonFilePath = commandsDataAccessor.GetCommandsFile().FullName;
+    }
+
+    public string CommandsJsonFilePath
+    {
+      get => _commandsJsonFilePath;
+      set
+      {
+        _commandsJsonFilePath = value;
+        RaisePropertyChanged(nameof(CommandsJsonFilePath));
+      }
+    }
+
+    public ICommand SaveCommand => new DelegateCommand<Window>(window =>
+      {
+        _commandsDataAccessor.ChangeCommandsJsonFilePath(CommandsJsonFilePath);
+        window.Close();
+      }
+    );
+
+    public ICommand CancelCommand => new DelegateCommand<Window>(window => window.Close());
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void RaisePropertyChanged(string propertyName)
+    {
+      var handler = PropertyChanged;
+      handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
