610190ed9770862ab8e0982d1a3cd8b7eb9d5e68
Matthias Lischka <matthias.lischka@gmx.at>
2017-07-30 22:00:43 +0200

    fixed double click on datagrid issue

--- StartLauncher.App/Views/MainWindow.xaml.cs
+++ StartLauncher.App/Views/MainWindow.xaml.cs
@@ -17,7 +17,6 @@ namespace StartLauncher.App.Views
                 PropertyName = "Name",
                 Direction = ListSortDirection.Ascending
             });
-            CommandsListView.Columns[0].SortDirection = ListSortDirection.Ascending;
 
             InitializeTrayNotifyIcon();
         }
