25af8f3cae3217817a8fc016d5ece1a1339e2ce3
Matthias Lischka <matthias.lischka@signon-group.com>
2019-06-04 18:19:57 +0200

    fix shutdown issue

--- StartLauncher.App/App.xaml.cs
+++ StartLauncher.App/App.xaml.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -14,110 +13,94 @@ using StartLauncher.App.Views;
 
 namespace StartLauncher.App
 {
-    public partial class App
-    {
-        public static ManualResetEvent UpdateCheckResetEvent = new ManualResetEvent(false);
-        private static readonly Mutex SingleInstanceApplicationMutex = new Mutex(true,
-            "{333499E4-B949-48F2-8C7C-6DFBF11ED9E1}");
+	public partial class App
+	{
+		private readonly ICommandsDataAccessor _commandsDataAccessor;
+		private readonly IExecutablesAccessor _executablesAccessor;
 
-        private readonly ICommandsDataAccessor _commandsDataAccessor;
-        private readonly IExecutablesAccessor _executablesAccessor;
+		public App(IExecutablesAccessor executablesAccessor, ICommandsDataAccessor commandsDataAccessor)
+		{
+			Startup += App_Startup;
 
-        public App(IExecutablesAccessor executablesAccessor, ICommandsDataAccessor commandsDataAccessor)
-        {
-            EnsureApplicationRunsJustOnce();
+			_executablesAccessor = executablesAccessor;
+			_commandsDataAccessor = commandsDataAccessor;
+		}
 
-            _executablesAccessor = executablesAccessor;
-            _commandsDataAccessor = commandsDataAccessor;
-        }
+		[STAThread]
+		public static void Main()
+		{
+			EnsureAppUpToDate().ContinueWith(t => Console.Error.WriteLine(t.Exception),
+				TaskContinuationOptions.OnlyOnFaulted);
 
-        private void EnsureApplicationRunsJustOnce()
-        {
-            if (SingleInstanceApplicationMutex.WaitOne(TimeSpan.Zero, true))
-            {
-                Startup += App_Startup;
-                SingleInstanceApplicationMutex.ReleaseMutex();
-            }
-            else
-            {
-                Shutdown();
-            }
-        }
+			var appContainerBuilder = new AppContainerBuilder();
+			var appContainer = appContainerBuilder.Build();
+			var application = appContainer.Resolve<App>();
+			application.InitializeComponent();
+			application.Run();
+		}
 
-        [STAThread]
-        public static void Main()
-        {
-            EnsureAppUpToDate().ContinueWith(t => Console.Error.WriteLine(t.Exception),
-                TaskContinuationOptions.OnlyOnFaulted);
+		private void App_Startup(object sender, StartupEventArgs e)
+		{
+			new MainWindow
+			{
+				DataContext = new MainViewModel(_commandsDataAccessor.Commands, _commandsDataAccessor, _executablesAccessor)
+			}.Show();
+		}
 
-            var appContainerBuilder = new AppContainerBuilder();
-            var appContainer = appContainerBuilder.Build();
-            var application = appContainer.Resolve<App>();
-            application.InitializeComponent();
-            application.Run();
-        }
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			_executablesAccessor.EnsureCommands();
+		}
 
-        private void App_Startup(object sender, StartupEventArgs e)
-        {
-            new MainWindow
-            {
-                DataContext = new MainViewModel(_commandsDataAccessor.Commands, _commandsDataAccessor, _executablesAccessor)
-            }.Show();
-        }
+		private static async Task EnsureAppUpToDate()
+		{
+			var assembly = Assembly.GetEntryAssembly();
+			var updateDotExe = Path.Combine(Path.GetDirectoryName(assembly.Location), "..", "Update.exe");
+			var isInstalled = File.Exists(updateDotExe);
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            _executablesAccessor.EnsureCommands();
-        }
+			//so you can run app from Dev Environment
+			if (!isInstalled)
+				return;
 
-        private static async Task EnsureAppUpToDate()
-        {
-            var assembly = Assembly.GetEntryAssembly();
-            var updateDotExe = Path.Combine(Path.GetDirectoryName(assembly.Location), "..", "Update.exe");
-            var isInstalled = File.Exists(updateDotExe);
+			var updated = false;
 
-            //so you can run app from Dev Environment
-            if (!isInstalled)
-                return;
+			using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/myuser/myapp"))
+			{
+				await mgr.Result.UpdateApp(i => Console.Out.WriteLine($"Updating: {i}"));
+			}
 
-            var updated = false;
+			using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/matthiaslischka/StartLauncher").Result)
+			{
+				var updateInfo = await mgr.CheckForUpdate();
+				if (updateInfo.ReleasesToApply.Any())
+				{
+					Console.Out.WriteLine($"Found Update {updateInfo.FutureReleaseEntry.Version}.");
+					await mgr.UpdateApp(i => Console.Out.WriteLine($"Updating: {i}"));
+					Console.Out.WriteLine("Update Finished.");
+					updated = true;
+				}
+			}
 
-            using (var mgr = UpdateManager
-                .GitHubUpdateManager("https://github.com/matthiaslischka/StartLauncher").Result)
-            {
-                var updateInfo = await mgr.CheckForUpdate();
-                if (updateInfo.ReleasesToApply.Any())
-                {
-                    System.Console.Out.WriteLine($"Found Update {updateInfo.FutureReleaseEntry.Version}.");
-                    await mgr.UpdateApp(i => System.Console.Out.WriteLine($"Updating: {i}"));
-                    System.Console.Out.WriteLine("Update Finished.");
-                    updated = true;
-                }
-            }
+			if (updated)
+			{
+				Console.Out.WriteLine("Restarting to launch new Version.");
+				UpdateManager.RestartApp();
+			}
+		}
 
-            if (updated)
-            {
-                System.Console.Out.WriteLine("Restarting to launch new Version.");
-                UpdateManager.RestartApp();
-            }
+		private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show(
+				"An unhandled exception just occurred.\nPlease report this issue to https://github.com/matthiaslischka/StartLauncher\nThx for contributing.\n\nSee StdErr output for more information.\n\n:)",
+				"Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+			Console.Error.WriteLine(e.Exception);
+			e.Handled = true;
+			Current.Shutdown();
+		}
 
-            UpdateCheckResetEvent.Set();
-        }
-
-        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            MessageBox.Show(
-                "An unhandled exception just occurred.\nPlease report this issue to https://github.com/matthiaslischka/StartLauncher\nThx for contributing.\n\nSee StdErr output for more information.\n\n:)",
-                "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
-            Console.Error.WriteLine(e.Exception);
-            e.Handled = true;
-            Current.Shutdown();
-        }
-
-        private void App_OnExit(object sender, ExitEventArgs e)
-        {
-            UpdateCheckResetEvent.WaitOne();
-        }
-    }
+		private void App_OnExit(object sender, ExitEventArgs e)
+		{
+		}
+	}
 }
